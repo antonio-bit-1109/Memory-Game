4a39f2b0c446e0e2ea412eda2e4aa112b25b346f
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-09-30 21:56:49 +0200

    memory logic done

--- script.js
+++ script.js
@@ -21,11 +21,24 @@ document.addEventListener("DOMContentLoaded", function () {
     ];
 
     // sequenza delle funzioni da chiamare.
+    createModal();
+    startClock();
     const imageArray = CreateArrayImgs(arrImg);
     const arrayMischiato = shuffleArray(imageArray);
     InsertImagesIntoBoxes(arrayMischiato);
     addListenerToBoxes(arrayTupla, arrayTemp, arrDomElem);
 });
+
+function startClock() {
+    let clock = document.getElementById("clock");
+
+    let sec = 0;
+    let min = 0;
+    setInterval(() => {
+        sec++;
+        clock.innerHTML = `0${min}:0${sec}`;
+    }, 1000);
+}
 
 // creo un array di immagini
 function CreateArrayImgs(arrImg) {
@@ -99,45 +112,9 @@ function DoStuff(e, box, arrayTupla, arrayTemp, arrDomElem) {
     }
 
     if (arrayTupla.length === 2) {
-        let id = controlloEsito(arrayTupla, arrDomElem);
-        // id && controlloSeClassiTolte(arrDomElem, id);
+        controlloEsito(arrayTupla, arrDomElem);
     }
-    // let alt = mostraCard(e, box);
-    // if (arrayTupla.length >= 2) {
-    //     console.log("sto pushando in temp");
-    //     arrayTemp.push(alt);
-    //     console.log(arrayTemp);
-    // }
-    // if (arrayTupla.length < 2) {
-    //     arrayTupla.push(alt);
-    //     console.log("array tupla length inferiore a due. alt inserito in arrayTupla");
-    //     console.log(arrayTupla);
-    //     return;
-    // }
-    // if (arrayTupla.length === 2) {
-    //     if (arrayTupla[0] === arrayTupla[1]) {
-    //         console.log("match trovato!");
-    //         handleModal();
-    //         resetArray(arrayTupla, arrayTemp);
-    //         return;
-    //     }
-    //     if (arrayTupla[0] !== arrayTupla[1]) {
-    //         // console.log("match trovato!");
-    //         // reset(arrayTupla);
-    //         resetCards(arrayTupla);
-    //         resetArray(arrayTupla, arrayTemp);
-    //         return;
-    //     }
-    // }
 }
-
-// function controlloSeClassiTolte(arrayDomElem, id) {
-//     arrayDomElem.forEach((box) => {
-//         if (!box.classList.contains("flip") && box.querySelector("img").classList.contains("invisible")) {
-//             clearTimeout(id);
-//         }
-//     });
-// }
 
 function flippaCard(e) {
     const box = e.currentTarget;
@@ -160,14 +137,20 @@ function controlloEsito(arrayTupla, arrDomElem) {
     const [first, second] = arrayTupla;
     if (first === second) {
         console.log("hai trovato le due card uguali.");
+        showModal();
+        unShowModal();
         resetArray(arrayTupla);
+        resetArray(arrDomElem);
         return;
-    } else {
+    }
+
+    if (first !== second) {
         console.log("non hai trovato le due card uguali.");
         setTimeout(() => {
             resetCard(arrDomElem);
+            resetArray(arrayTupla);
+            resetArray(arrDomElem);
         }, 800);
-        // return id;
     }
 }
 
@@ -182,85 +165,27 @@ function resetCard(arrDomElem) {
         card.classList.add("invisible");
     });
 }
-// function mostraCard(e, box) {
-//     let image = e.currentTarget.querySelector("img");
-//     console.log(image);
-//     if (image) {
-//         image.classList.remove("invisible");
-//         box.classList.add("flip");
-//         console.log(image.alt);
-//         return image.alt;
-//     }
-// }
 
-// function resetArray(arrayTupla, arrayTemp) {
-//     // arrayTupla.splice(0, arrayTupla.length);
-//     arrayTupla.length = 0;
-//     console.log(arrayTupla);
-
-//     if (arrayTemp.length === 1) {
-//         console.log("sposto alt in array temp dentro arraytupla");
-//         arrayTupla.push(...arrayTemp);
-//     }
-//     console.log(arrayTupla);
-//     console.log(arrayTemp);
-//     arrayTemp.length = 0;
-// }
-
-// trovare elementi dom img che poosseggono il valore di alt specificato
-// all img trovata ridare la classe invisible
-// trovare il suo box parent e rimuovere l animazione flip
-
-// function resetCards(arrayTupla) {
-//     let imgDomElem1 = document.querySelectorAll(`[alt='${arrayTupla[0]}']`);
-//     let imgDomElem2 = document.querySelectorAll(`[alt='${arrayTupla[1]}']`);
-
-//     imgDomElem1.forEach((img) => {
-//         let box = img.closest(".box"); // Trova il genitore .box
-//         if (box && box.classList.contains("flip")) {
-//             // Verifica se il genitore ha la classe flip
-//             box.classList.remove("flip"); // Rimuovi la classe flip
-//             let childImg = box.querySelector(`[alt='${arrayTupla[0]}']`);
-//             if (childImg) {
-//                 childImg.classList.add("invisible"); // Rendi invisibile l'immagine
-//             }
-//         }
-//     });
-
-//     imgDomElem2.forEach((img) => {
-//         let box = img.closest(".box"); // Trova il genitore .box
-//         if (box && box.classList.contains("flip")) {
-//             // Verifica se il genitore ha la classe flip
-//             box.classList.remove("flip"); // Rimuovi la classe flip
-//             let childImg = box.querySelector(`[alt='${arrayTupla[1]}']`);
-//             if (childImg) {
-//                 childImg.classList.add("invisible"); // Rendi invisibile l'immagine
-//             }
-//         }
-//     });
-// }
-
-function handleModal() {
-    let modal;
-    if (document.getElementById("modal")) {
-        null;
-    } else {
-        modal = document.createElement("div");
-    }
-
+function createModal() {
+    const modal = document.createElement("div");
     modal.id = "modal";
-    showModal(modal);
+    modal.classList.add("modalStyle", "d-none");
+    modal.innerHTML = "WELL DONE!";
+
+    document.body.appendChild(modal);
+    console.log("modale creato");
+}
+
+function showModal() {
+    let modal = document.getElementById("modal");
+    if (modal) {
+        modal.classList.remove("d-none");
+    }
+}
+
+function unShowModal() {
+    let modal = document.getElementById("modal");
     setTimeout(() => {
-        unShowModal(modal);
+        modal.classList.add("d-none");
     }, 1500);
-}
-
-function showModal(modal) {
-    modal.classList.contains("modalStyle") ? null : modal.classList.add("modalStyle");
-    modal.innerHTML === "WELL DONE!" ? null : (modal.innerHTML = "WELL DONE!");
-    document.body.contains(modal) ? null : document.body.appendChild(modal);
-}
-
-function unShowModal(modal) {
-    modal.classList.add("d-none");
 }
